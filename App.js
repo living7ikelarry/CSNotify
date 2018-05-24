9c01a331e9e12a6d8424ee530ad306a02f517618
Brian Hoernschemeyer <hoernsbj@brianhoyersimac.tucs.uc.edu>
2018-05-24 16:36:32 -0400

    fixed icons. also edited api request

--- App.js
+++ App.js
@@ -7,10 +7,13 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  Dimensions
 } from 'react-native';
 import t from 'tcomb-form-native'
 import ImageFactory from 'react-native-image-picker-form'
+
+const { width } = Dimensions.get('window');
 
 const Location = t.enums({
   'MSH': 'Marian Spencer Hall',
@@ -67,7 +70,7 @@ export default class App extends React.Component<Props, State> {
 
           image: {
             config: {
-              title: 'Select image',
+              title: 'Take Picture',
               options: ['Open camera', 'Select from gallery', 'Cancel'],
               // Used on Android to style BottomSheet
               style: {
@@ -86,30 +89,45 @@ export default class App extends React.Component<Props, State> {
   onPress() {
     var value = this._formRef.getValue();
     if (value) { // if validation fails, value will be null
-      console.log(value.location); // value here is an instance of Person
-      fetch('http://10.142.2.167:3000/tickets', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          location: value.location,
-          category: value.category,
-          description: value.description
-        }),
-      })
+      var image = {
+        uri: value.image,
+        type: 'image/jpeg',
+        name: 'img.jpg',
+      };
+      var data = new FormData();
+      data.append("image", image);
+      data.append("category", "BCC");
+      data.append("description", "reader down");
+      data.append("location", "CRC");
+
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+
+      xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+          console.log(this.responseText);
+        }
+      });
+
+      xhr.open("POST", "http://10.142.2.167:3000/tickets");
+      xhr.setRequestHeader("Cache-Control", "no-cache");
+      xhr.setRequestHeader("Postman-Token", "08bd658d-1f02-43e2-8f2e-640fc0fe14dc");
+
+      xhr.send(data);
     }
   }
 
   render() {
     return (
       <ScrollView>
-        <View>
-          <Image
-            style={{width: 369, height: 127}}
-            source={{uri: 'https://cso.uc.edu/CSO.jpg'}}
-          />
+        <View style={styles.container}>
+          <View style={styles.bgContainer}>
+            <Image
+              style={styles.image}
+              resizeMode='contain'
+              source={require('./images/cso.jpg')}
+            />
+          </View>
           <Form
             ref={(ref) => this._formRef=ref}
             type={DocumentFormStruct}
@@ -128,9 +146,9 @@ export default class App extends React.Component<Props, State> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff',
+    flexDirection:'column',
+    margin: 10
   },
   button: {
     marginTop: 20,
@@ -140,5 +158,15 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#ffffff',
-  }
+  },
+  image: {
+   width: 250,
+   height: 86,
+ },
+ bgContainer: {
+   width: width-5,
+   height: 86,
+   justifyContent: 'flex-start',
+   alignItems: 'center'
+ }
 });
